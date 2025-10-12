6b55f03de5e9708941226506be17fbc5997b1eac
Flip Van de Voort <106674166+fvoort@users.noreply.github.com>
2025-10-12 12:22:15 +0200

    Added symlink to Karabiner config

--- karabiner.sh
+++ karabiner.sh
@@ -5,8 +5,9 @@ echo "Setting up Karabiner Elements..."
 # Delete the default '~/.config/karabiner' folder
 rm -rf ~/.config/karabiner
 
-# Clone Git repository
-git clone git@github.com:fvoort/karabiner.git ~/.config
+# Clone Git repository and symlink it
+git clone git@github.com:fvoort/karabiner.git /Users/fvoort/Documents/Git/karabiner
+ln -sf /Users/fvoort/Documents/Git/karabiner ~/.config/karabiner
 
 # Restart karabiner_console_user_server
 launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
